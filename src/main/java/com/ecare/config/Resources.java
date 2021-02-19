fec89a3dfb3fcc9eaa576dcc26be516c02432033
Denis Verkhovskii <denis.verkhovskii@t-systems.com>
2021-02-19 22:12:47 +0300

    add comments

--- src/main/java/com/ecare/config/Resources.java
+++ src/main/java/com/ecare/config/Resources.java
@@ -8,12 +8,21 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.enterprise.inject.Produces;
 import javax.inject.Qualifier;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
+/**
+ * Defines resources of the project
+ */
 @Stateful
 @RequestScoped
 public class Resources {
 
+    /**
+     * @return FacesContext for the application
+     */
     @Produces
     public FacesContext getFacesContext() {
         return FacesContext.getCurrentInstance();
--- src/test/java/com/ecare/tests/TestParsers.java
+++ src/test/java/com/ecare/tests/TestParsers.java
@@ -2,21 +2,30 @@ package com.ecare.tests;
 
 import com.ecare.network.DataChangeNotification;
 import com.ecare.network.MsgConverter;
+import com.ecare.plans.Plan;
+import com.ecare.plans.PlanDeserializer;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.mockrunner.mock.jms.MockSession;
 import com.mockrunner.mock.jms.MockQueueConnection;
 import com.mockrunner.jms.DestinationManager;
 import com.mockrunner.jms.ConfigurationManager;
+import org.mockito.Mock;
+import org.springframework.web.client.RestTemplate;
 
 import javax.jms.Message;
 import javax.jms.Session;
+import java.io.StringReader;
 
 public class TestParsers {
+
+    @Mock
+    private RestTemplate restTemplate;
 
     @Test
     public void testParseNotification() throws Exception {
@@ -37,6 +46,24 @@ public class TestParsers {
         Message msgSerialized = converter.toMessage(msg, session);
         DataChangeNotification parsed = (DataChangeNotification) converter.fromMessage(msgSerialized);
 
-        assertEquals(parsed, msg);
+        assertEquals(msg, parsed);
     }
+    
+    @Test
+    public void testJsonParser() throws Exception {
+        
+        String data = "{ \"id\":1, \"name\":\"Dictator\", \"price\":1000.0, \"options\":[ { \"attachedId\":1, \"id\":1, \"name\":\"Block everyone\", \"price\":10.0, \"description\":\"Whenever you want you can block the Internet in the whole country. Make them suffer\", \"turnOnPrice\":1.0, \"undisablable\":true, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ { \"id\":1, \"optionId1\":1, \"optionId2\":5, \"type\":\"INCOMPATIBLE\" } ] }, { \"attachedId\":2, \"id\":2, \"name\":\"Internet\", \"price\":0.0, \"description\":\"Use the magic to communicate with other people vie chats and send the catpics\", \"turnOnPrice\":1.0, \"undisablable\":false, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ { \"id\":2, \"optionId1\":2, \"optionId2\":5, \"type\":\"INCOMPATIBLE\" } ] }, { \"attachedId\":3, \"id\":3, \"name\":\"Calls\", \"price\":0.0, \"description\":\"Just take your phone and click call!\", \"turnOnPrice\":1.0, \"undisablable\":false, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ { \"id\":3, \"optionId1\":3, \"optionId2\":5, \"type\":\"INCOMPATIBLE\" } ] }, { \"attachedId\":4, \"id\":4, \"name\":\"Spam-suffer\", \"price\":0.0, \"description\":\"Use this opportunity to make your peasants suffer!\", \"turnOnPrice\":1.0, \"undisablable\":true, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ { \"id\":4, \"optionId1\":4, \"optionId2\":5, \"type\":\"INCOMPATIBLE\" } ] }, { \"attachedId\":5, \"id\":6, \"name\":\"News\", \"price\":0.0, \"description\":\"Receive news everyday\", \"turnOnPrice\":1.0, \"undisablable\":false, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ ] }, { \"attachedId\":6, \"id\":7, \"name\":\"Bieber\", \"price\":0.0, \"description\":\"Let them suffer listening Justin Bieber\", \"turnOnPrice\":1.0, \"undisablable\":false, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ ] }, { \"attachedId\":7, \"id\":8, \"name\":\"You don't pay\", \"price\":0.0, \"description\":\"Ley your friend pay for your call\", \"turnOnPrice\":1.0, \"undisablable\":false, \"deprecated\":false, \"enabled\":false, \"restrictions\":[ ] } ] }";
+        StringReader reader = new StringReader(data);
+        JsonParser jParser = new JsonFactory().createParser(data);
+        ObjectMapper mapper = new ObjectMapper();
+        jParser.setCodec(mapper);
+        PlanDeserializer parser = new PlanDeserializer();
+        Plan plan = parser.deserialize(jParser, null);
+
+        assertEquals("Dictator", plan.getName());
+        assertEquals((Double)1000.0, plan.getPrice());
+        assertEquals((Long)1l, plan.getId());
+        assertEquals(7, plan.getOptions().size());
+    }
+    
 }
